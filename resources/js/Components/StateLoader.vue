6a9ce8a0de48181d6293d262b9774b3c2eb050d1
ssalpo <ssalpomishevich@gmail.com>
2024-06-23 08:51:13 +0500

    preloader component

---
+++ resources/js/Components/StateLoader.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import { router } from '@inertiajs/vue3'
+
+const isLoading = ref(0);
+
+onMounted(() => {
+    router.on('start', () => isLoading.value = true)
+
+    router.on('finish', () => isLoading.value = false)
+})
+
+</script>
+
+<template>
+    <div class="state-loader" v-if="isLoading">
+        <div class="loader"></div>
+    </div>
+</template>
--- resources/js/Layouts/BaseLayout.vue
+++ resources/js/Layouts/BaseLayout.vue
@@ -1,9 +1,12 @@
 <script setup>
 import InternetStateChecker from "@/Components/InternetStateChecker.vue";
+import StateLoader from "@/Components/StateLoader.vue";
 </script>
 
 <template>
     <internet-state-checker />
+
+    <state-loader />
 
     <slot />
 </template>
