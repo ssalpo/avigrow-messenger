5d90f25f70025fc9afba3888391227b5714f541d
ssalpo <ssalpomishevich@gmail.com>
2024-09-04 18:02:03 +0500

    pwa settings

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     build: {
@@ -13,6 +14,7 @@ export default defineConfig({
         }
     },
     plugins: [
+        VitePWA({ registerType: 'autoUpdate' }),
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
