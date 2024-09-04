d38536653a546efe9e247d809a0283128331342c
ssalpo <ssalpomishevich@gmail.com>
2024-09-04 17:49:53 +0500

    hotfix

--- resources/js/Components/InternetStateChecker.vue
+++ resources/js/Components/InternetStateChecker.vue
@@ -1,17 +1,40 @@
 <script setup>
 import {onMounted, ref} from "vue";
 
-let isOnline = ref(true);
+let dialog = ref(false);
 
 onMounted(() => {
-    window.addEventListener('online', () => isOnline.value = true);
-    window.addEventListener('offline', () => isOnline.value = false);
+    window.addEventListener('online', () => dialog.value = false);
+    window.addEventListener('offline', () => dialog.value = true);
 });
 </script>
 
 <template>
-    <div class="internet-status-checker" v-if="!isOnline">
-        <span>Интернет отключен</span>
-        <small>Окно автоматически закроется после подключения к интернету!</small>
-    </div>
+    <v-dialog
+        v-model="dialog"
+        persistent
+    >
+        <v-list
+            class="py-2"
+            color="primary"
+            elevation="12"
+            rounded="lg"
+        >
+            <v-list-item
+                prepend-icon="$vuetify-outline"
+                title="Интернет отключен"
+                subtitle="Окно автоматически закроется после подключения к интернету!"
+            >
+                <template v-slot:prepend>
+                    <div class="pe-4">
+                        <v-icon color="primary" size="x-large">mdi-connection</v-icon>
+                    </div>
+                </template>
+
+                <template v-slot:subtitle="{subtitle}">
+                    <div style="-webkit-line-clamp: 0;">{{subtitle}}</div>
+                </template>
+            </v-list-item>
+        </v-list>
+    </v-dialog>
 </template>
