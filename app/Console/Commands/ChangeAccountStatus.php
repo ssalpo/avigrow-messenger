3121649faa76b7569671fc9d0a2dddca0a02f324
ssalpo <ssalpomishevich@gmail.com>
2024-09-08 16:35:43 +0500

    смена статуса онлайн

---
+++ app/Console/Commands/ChangeAccountStatus.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Account;
+use App\Services\Avito;
+use Illuminate\Console\Command;
+
+class ChangeAccountStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:change-account-status';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Команда затычка, чтобы менять статус аккаунта на онлайн';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(Avito $avito)
+    {
+        $accounts = Account::all();
+
+        $accounts->each(function ($account) use($avito) {
+            $avito->setAccount($account)->me();
+        });
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('app:update-expired-tokens')->everyMinute();
 Schedule::command('app:send-scheduled-reviews')->dailyAt('19:05');
+Schedule::command('app:change-account-status')->everyFiveMinutes();
