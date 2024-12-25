3081bc74f4c6ad96d98b1e64be28ed1b585f8034
ssalpo <ssalpomishevich@gmail.com>
2024-12-25 11:21:48 +0500

    hotfix

--- resources/js/Components/ConversationTabs.vue
+++ resources/js/Components/ConversationTabs.vue
@@ -17,16 +17,16 @@ function goToItem(url) {
 </script>
 
 <template>
-    <v-sheet color="deep-purple-darken-4" class="px-3 py-2 d-flex align-center justify-between" style="z-index: 1">
+    <v-sheet color="blue-darken-1" class="px-3 py-2 d-flex align-center justify-between" style="z-index: 1">
         <v-icon icon="mdi-chat-processing-outline" @click="() => router.visit(`/accounts/${activeAccount.id}/chats/`)"/>
 
-        <v-sheet v-if="chat" color="deep-purple-darken-4" class="ml-3" max-width="220" style="font-size: 12px">
-            <v-sheet color="deep-purple-darken-4" class="mb-2 text-truncate pr-3">{{ chat.user.name }}</v-sheet>
+        <v-sheet v-if="chat" color="blue-darken-1" class="ml-3" max-width="220" style="font-size: 12px">
+            <v-sheet color="blue-darken-1" class="mb-2 text-truncate pr-3">{{ chat.user.name }}</v-sheet>
             <v-sheet
                 @click="goToItem(chat.url)"
-                color="deep-purple-darken-4" class="pr-3 text-truncate d-flex">
-                <v-sheet color="deep-purple-darken-4" class="pr-3 text-truncate">{{ chat.context }}</v-sheet>
-                <v-sheet color="deep-purple-darken-4" class="pr-3">{{ chat.price }}</v-sheet>
+                color="blue-darken-1" class="pr-3 text-truncate d-flex">
+                <v-sheet color="blue-darken-1" class="pr-3 text-truncate">{{ chat.context }}</v-sheet>
+                <v-sheet color="blue-darken-1" class="pr-3">{{ chat.price }}</v-sheet>
             </v-sheet>
         </v-sheet>
 
