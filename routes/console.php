528570290b9d780b60699ce6706d290d49c53687
ssalpo <ssalpomishevich@gmail.com>
2025-01-16 15:00:33 +0500

    gemini disabled

--- routes/console.php
+++ routes/console.php
@@ -11,5 +11,5 @@ Schedule::command('app:clear-unused-fm-tags')->dailyAt('1:00');
 Schedule::command('app:close-inactive-bot-states')->daily();
 Schedule::command('app:import-ads-for-accounts')->weekly()->saturdays()->at('3:00');
 Schedule::command('app:import-ads-for-accounts')->dailyAt('3:00');
-Schedule::command('app:import-reviews')->dailyAt('2:00');
-Schedule::command('app:process-review-answers')->hourly();
+//Schedule::command('app:import-reviews')->dailyAt('2:00');
+//Schedule::command('app:process-review-answers')->hourly();
