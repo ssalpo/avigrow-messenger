653b37fc0cc93ad039bd36178a9e9ad546709852
ssalpo <ssalpomishevich@gmail.com>
2025-01-19 07:23:14 +0500

    ad importer hotfix

--- app/Services/Ads/AdImporter.php
+++ app/Services/Ads/AdImporter.php
@@ -5,6 +5,7 @@ namespace App\Services\Ads;
 use App\Models\Account;
 use App\Models\Ad;
 use App\Services\Avito;
+use mysql_xdevapi\Exception;
 
 class AdImporter
 {
@@ -21,25 +22,36 @@ class AdImporter
         $this->avito->setAccount($account);
 
         do {
-            if($sleep) {
+            if ($sleep) {
                 sleep(random_int(2, 4));
             }
+
 
             $data = $this->avito->getItems(page: $page);
 
             $resources = $data['resources'] ?? [];
 
             foreach ($resources as $resource) {
-                Ad::updateOrInsert([
-                    "account_id" => $account->id,
-                    "external_id" => $resource['id']
-                ], [
-                    "price" => $resource['price'] ?? 0,
-                    "title" => $resource['title'],
-                    "url" => $resource['url'],
-                    'created_at' => now()
-                ]);
+                try {
+                    Ad::updateOrInsert([
+                        "account_id" => $account->id,
+                        "external_id" => $resource['id']
+                    ], [
+                        "price" => $resource['price'] ?? 0,
+                        "title" => $resource['title'],
+                        "url" => $resource['url'],
+                        'created_at' => now()
+                    ]);
+                } catch (\Exception $e) {
+
+                    logger()?->error(
+                        json_encode($resource, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
+                    );
+
+                    throw new Exception($e->getMessage(), 0, $e);
+                }
             }
+
 
             $page++;
         } while (!empty($resources));
