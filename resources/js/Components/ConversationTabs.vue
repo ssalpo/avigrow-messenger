c8671d641f89cebf0c3b14b98720b0d42fe3e15b
ssalpo <ssalpomishevich@gmail.com>
2025-01-19 15:46:01 +0500

    message tab hotfix

--- resources/js/Components/ConversationTabs.vue
+++ resources/js/Components/ConversationTabs.vue
@@ -17,28 +17,31 @@ function goToItem(url) {
 </script>
 
 <template>
-    <v-sheet color="blue-darken-1" class="px-3 py-2 d-flex align-center justify-between" style="z-index: 1">
-        <v-icon icon="mdi-arrow-left" @click="() => router.visit(`/accounts/${activeAccount.id}/chats/`)"/>
+    <div>
+        <v-sheet color="blue-darken-1" class="px-3 py-2 d-flex align-center justify-between" style="z-index: 1">
+            <v-icon icon="mdi-arrow-left" @click="() => router.visit(`/accounts/${activeAccount.id}/chats/`)"/>
 
-        <v-sheet v-if="chat" color="blue-darken-1" class="ml-3 text-truncate" max-width="220" :style="{fontSize: '15px'}" >
-            {{ chat.user.name }}
+            <v-sheet v-if="chat" color="blue-darken-1" class="ml-3 text-truncate" max-width="220" :style="{fontSize: '15px'}" >
+                {{ chat.user.name }}
+            </v-sheet>
+
+            <v-spacer/>
+
+            <active-conversation-list
+                :active-account="activeAccount"
+                icon="mdi-forum-outline"
+            />
         </v-sheet>
 
-        <v-spacer/>
-
-        <active-conversation-list
-            :active-account="activeAccount"
-            icon="mdi-forum-outline"
-        />
-    </v-sheet>
-
-    <v-sheet
-        v-if="chat.context !== ''"
-        @click="goToItem(chat.url)"
-        color="blue-darken-1" class="pr-3 px-4 py-2 text-truncate d-flex" style="border-top: 1px solid #ffffff8f; font-size: 12px">
-        <v-sheet color="blue-darken-1" class="pr-3 text-truncate">{{ chat.context }}</v-sheet>
-        <v-sheet color="blue-darken-1" class="pr-3">{{ chat.price }}</v-sheet>
-    </v-sheet>
+        <v-sheet
+            v-if="chat.context !== ''"
+            @click="goToItem(chat.url)"
+            color="blue-darken-1" class="pr-3 px-4 py-2 text-truncate d-flex" style="border-top: 1px solid #ffffff8f; font-size: 12px">
+            <v-sheet color="blue-darken-1" class="pr-3 text-truncate">{{ chat.context }}</v-sheet>
+            <v-spacer/>
+            <v-sheet color="blue-darken-1" class="pr-3">{{ chat.price }}</v-sheet>
+        </v-sheet>
+    </div>
 </template>
 
 <style scoped>
