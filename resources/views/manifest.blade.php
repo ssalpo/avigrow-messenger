4e3a9ee13c79fd9d810b3c88de83531b15ea9ecf
ssalpo <ssalpomishevich@gmail.com>
2025-02-05 11:18:38 +0500

    pwa config

--- resources/views/manifest.blade.php
+++ resources/views/manifest.blade.php
@@ -1,5 +1,5 @@
 {
-    "name": "Авито ЧАТ",
+    "name": "AviGrow",
     "start_url": "/",
     "display": "standalone",
     "background_color": "#ffffff",
@@ -9,100 +9,16 @@
     "orientation": "portrait",
     "icons": [
         {
-            "src": "/icons/icon_72.png",
-            "sizes": "72x72",
-            "type": "image/png",
-            "purpose": "any"
-        },
-        {
-            "src": "/icons/icon_96.png",
-            "sizes": "96x96",
-            "type": "image/png",
-            "purpose": "any"
-        },
-        {
-            "src": "/icons/icon_128.png",
-            "sizes": "128x128",
-            "type": "image/png",
-            "purpose": "any"
-        },
-        {
-            "src": "/icons/icon_144.png",
-            "sizes": "144x144",
-            "type": "image/png",
-            "purpose": "any"
-        },
-        {
-            "src": "/icons/icon_152.png",
-            "sizes": "152x152",
-            "type": "image/png",
-            "purpose": "any"
-        },
-        {
-            "src": "/icons/icon_192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any"
-        },
-        {
-            "src": "/icons/icon_384.png",
-            "sizes": "384x384",
-            "type": "image/png",
-            "purpose": "any"
-        },
-        {
-            "src": "/icons/icon_512.png",
+            "purpose": "maskable",
             "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any"
+            "src": "/icons/icon512_maskable.png",
+            "type": "image/png"
         },
         {
-            "src": "/icons/maskable_72.png",
-            "sizes": "72x72",
-            "type": "image/png",
-            "purpose": "maskable"
-        },
-        {
-            "src": "/icons/maskable_96.png",
-            "sizes": "96x96",
-            "type": "image/png",
-            "purpose": "maskable"
-        },
-        {
-            "src": "/icons/maskable_128.png",
-            "sizes": "128x128",
-            "type": "image/png",
-            "purpose": "maskable"
-        },
-        {
-            "src": "/icons/maskable_144.png",
-            "sizes": "144x144",
-            "type": "image/png",
-            "purpose": "maskable"
-        },
-        {
-            "src": "/icons/maskable_152.png",
-            "sizes": "152x152",
-            "type": "image/png",
-            "purpose": "maskable"
-        },
-        {
-            "src": "/icons/maskable_192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
-        },
-        {
-            "src": "/icons/maskable_384.png",
-            "sizes": "384x384",
-            "type": "image/png",
-            "purpose": "maskable"
-        },
-        {
-            "src": "/icons/maskable_512.png",
+            "purpose": "any",
             "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
+            "src": "/icons/icon512_rounded.png",
+            "type": "image/png"
         }
     ]
 }
